3ff936d8904cc240b134a0e6ac4fc67a1a23d163
geonwoo <gudrjsdn8825@naver.com>
2025-05-07 19:40:49 +0900

    250507_C++ 수업 내용 학습

---
+++ Cpp_class_1/test01.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // 라이브러리 헤더
+using namespace std; // 라이브러리 헤더
+
+int main(){
+    cout << "Hello world!" << endl; // 기본 출력 방법
+    return 0;
+}
---
+++ Cpp_class_1/test02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "first C++ program\n";
+
+    int num;
+    cout << "input integer : ";
+    cin >> num; // cin : 입력받을 때 사용
+    cout << "inputed integer: " << num << "입니다.\n";
+
+    return 0;
+}
