56ea0137ab02ea0df70097f0f5611f916a499a47
geonwoo <gudrjsdn8825@naver.com>
2025-05-14 08:55:57 +0900

    250513_c++ 키워드 내용 추가

--- Cpp_class_5/class_13_move_semantics.cpp
+++ Cpp_class_5/class_13_move_semantics.cpp
@@ -14,7 +14,7 @@ public:
 };
 
 Integer Add(const Integer& a, const Integer& b) {
-	Integer tmp; // default 생성자로 생성되고 지역변수 객체이므로 임시객체로 봄
+	Integer tmp; // default 생성자로 생성되고 지역변수 객체
 	tmp.val = new int(a.val + b.val); 
 	// tmp 는 지역변수 니까 return 전 소멸자 실행으로 에러발생
 	return tmp;
