2f99cea89ffc818b3bfbeafd64e0127d59b7dd69
geonwoo <gudrjsdn8825@naver.com>
2025-06-05 00:21:50 -0400

    linux 프로그래밍(gcc 컴파일, makefile 기초)

--- linux_class_1/gcc_helloworld/helloworld.s
+++ linux_class_1/gcc_helloworld/helloworld.s
@@ -1,8 +1,8 @@
-	.file	"hello.c"
+	.file	"helloworld.c"
 	.text
 	.section	.rodata
 .LC0:
-	.string	"Hello World"
+	.string	"Hello, world!"
 	.text
 	.globl	main
 	.type	main, @function
@@ -15,11 +15,14 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
 	leaq	.LC0(%rip), %rax
 	movq	%rax, %rdi
 	call	puts@PLT
 	movl	$0, %eax
-	popq	%rbp
+	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
